902047ab11a6af8eebc401564a5be3485309d533
Robert Ableson <robertableson@gmail.com>
2017-02-10 21:36:41 -0500

    Ajout des 4 boutons principaux et leur méthode correspondante

--- app/src/main/java/com/example/rob/tp2androidrobertableson/MainActivity.java
+++ app/src/main/java/com/example/rob/tp2androidrobertableson/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.rob.tp2androidrobertableson;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openClickWar(View view){
+        Log.d("allo", "click war");
+    }
+
+    public void openLaPetiteDevinette(View view){
+        Log.d("allo", "la petite devinette");
+    }
+
+    public void openLuniversMeRepondra(View view){
+        Log.d("allo", "Allo L'univers!!!!!!!!!!!!!");
+    }
+
+    public void gameOver(View view){
+        Log.d("allo", "game over");
+    }
+
 }
