4a71a4c335a13f7d24db92d02a653a7c78b2e9da
Robert Ableson <robertableson@gmail.com>
2017-02-22 20:59:55 -0500

    clickwar timer is done

--- app/src/main/java/com/example/rob/tp2androidrobertableson/ClickWar.java
+++ app/src/main/java/com/example/rob/tp2androidrobertableson/ClickWar.java
@@ -1,13 +1,107 @@
 package com.example.rob.tp2androidrobertableson;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ClickWar extends AppCompatActivity {
+
+    int m_NombreClics;
+    boolean m_JeuEnCours,  m_CountdownTimerFini, m_BoutonActif;
+    TextView m_LblInfo;
+    TextView m_LblNombreClics;
+    TextView m_LblTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_click_war);
+
+        m_NombreClics = 0;
+        m_JeuEnCours = false;
+        m_BoutonActif = true;
+        m_CountdownTimerFini = false;
+        m_LblInfo = (TextView) findViewById(R.id.lblInfo);
+        m_LblNombreClics = (TextView) findViewById(R.id.lblNombreClics);
+        m_LblTimer = (TextView) findViewById(R.id.lblTimer);
+    }
+
+    public void boutonClic(View view){
+        //démarrage du jeu
+        if(!m_JeuEnCours && m_BoutonActif){
+            m_JeuEnCours = true;
+            partirTimerCountdown();
+        }else{
+            if(m_CountdownTimerFini && m_BoutonActif){
+                m_NombreClics++;
+                m_LblNombreClics.setText("" + m_NombreClics);
+            }
+        }
+    }
+
+    private void partirTimerCountdown(){
+        new CountDownTimer(5000, 100) {
+            public void onTick(long millisUntilFinished) {
+                m_LblInfo.setText("La partie commence dans: " + (millisUntilFinished + 1000) / 1000);
+            }
+
+            public void onFinish() {
+                m_LblInfo.setText("Record à battre: 33");
+                m_CountdownTimerFini = true;
+                partirTimerJeu();
+            }
+        }.start();
+    }
+
+    private void partirTimerJeu(){
+        final int tempsJeuSec = 10;
+
+        new CountDownTimer(tempsJeuSec * 1000, 10) {
+            public void onTick(long millisUntilFinished) {
+                long sec = millisUntilFinished / 1000;
+                long milli = millisUntilFinished;
+
+                    m_LblTimer.setText("" + (tempsJeuSec - sec - 1) + "." + ((1000 - (millisUntilFinished - ((millisUntilFinished / 1000) * 1000))/* - (millisUntilFinished % (millisUntilFinished / 1000))*/)));
+                /*else
+                    m_LblTimer.setText("0." + (1 - millisUntilFinished));*/
+            }
+
+            public void onFinish() {
+                afficherScore();
+                m_JeuEnCours = false;
+                m_BoutonActif = false;
+                m_LblNombreClics.setText("" + m_NombreClics + " clics!");
+                m_LblTimer.setText("Terminé!");
+
+                new CountDownTimer(5 * 1000, 10) {
+                    public void onTick(long millisUntilFinished) {}
+
+                    public void onFinish() {
+                        reeinitialiserJeu();
+                    }
+                }.start();
+            }
+        }.start();
+    }
+
+    private void afficherScore(){
+
+    }
+
+    private void reeinitialiserJeu(){
+        m_LblInfo.setText("Cliquez le bouton pour commencer la partie.");
+        m_LblTimer.setText("00:00");
+        m_LblNombreClics.setText("");
+
+        m_NombreClics = 0;
+        m_JeuEnCours = false;
+        m_CountdownTimerFini = false;
+        m_BoutonActif = true;
     }
 }
--- app/src/main/java/com/example/rob/tp2androidrobertableson/MainActivity.java
+++ app/src/main/java/com/example/rob/tp2androidrobertableson/MainActivity.java
@@ -6,12 +6,35 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Socket m_clientSocket = null;
+    private DataInputStream m_inClient = null;
+    private PrintStream m_outClient = null;
+    private DataOutputStream m_DOS = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*try{
+            m_clientSocket = new Socket("196.168.2.18", 2000);
+           /* m_DOS = new DataOutputStream(m_clientSocket.getOutputStream());
+            m_DOS.writeUTF("coucou");
+            m_clientSocket.close();
+            Log.d("CONNECTÉ AU SERVEUR", "ALLO");
+        }
+        catch(Exception ex){
+            Log.d("ERROR", ex.getMessage());
+        }*/
+
+
     }
 
     public void openClickWar(View view){
